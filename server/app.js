8bc88c26e80c305604799934995d90fb5d4ab37f
rabbygit <rabbyhossain482@gmail.com>
2020-02-13 15:44:57 +0600

    :sparkels: Adding basic json!

---
+++ server/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const { notFound, errorHandler } = require('./middlewares');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//Our routes go here...
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to Community API! 🌈 💚'
+    });
+})
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
